350935a91315163d77d9d011a9ffc20ae2bed3fa
Aditya Ajmera <82447175+sanganakchatra@users.noreply.github.com>
2021-04-13 17:42:20 +0530

    Creating basic project infra

---
+++ src/main/java/com/futuristic/dine/out/DineOutApplication.java
@@ -0,0 +1,13 @@
+package com.futuristic.dine.out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futuristic/dine/out/DineOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futuristic.dine.out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
